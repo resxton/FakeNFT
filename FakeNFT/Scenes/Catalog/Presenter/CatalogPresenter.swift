2b2f113bb14358cf9dbb657bb95c8e81ab0f9786
Kirill Somov <gornomirov661@gmail.com>
2025-07-08 00:32:26 +0300

    Networking setup

---
+++ FakeNFT/Scenes/Catalog/Presenter/CatalogPresenter.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+// MARK: - CatalogPresenter
+
+final class CatalogPresenter: CatalogPresenterProtocol {
+  // MARK: - Public Properties
+
+  weak var view: CatalogViewProtocol?
+  var collectionsCount: Int {
+    collections.count
+  }
+
+  // MARK: - Private Properties
+
+  private var collections: [CollectionDomain] = []
+  private var sortingOption: SortingOption?
+
+  private let services: ServicesAssembly
+  private let router: CatalogRouterProtocol
+
+  // MARK: - Initializers
+
+  init(
+    servicesAssembly: ServicesAssembly,
+    router: CatalogRouterProtocol
+  ) {
+    services = servicesAssembly
+    self.router = router
+  }
+
+  // MARK: - Public Methods
+
+  func viewDidLoad() {
+    loadCollections()
+  }
+
+  func didSelectRow(at indexPath: IndexPath) {
+    let collection = collections[indexPath.row]
+    router.show(collection: collection)
+  }
+
+  func collection(at index: Int) -> CollectionViewModel {
+    collections[index].toViewModel()
+  }
+
+  func sortButtonTapped() {
+    view?.presentSortingOptions()
+  }
+
+  func didSelectSorting(option: SortingOption) {
+    guard option != sortingOption else {
+      sortingOption = nil
+      return
+    }
+    sortingOption = option
+  }
+
+  // MARK: - Private Methods
+
+  private func loadCollections() {
+    view?.showLoader()
+
+    services.collectionService.loadCollections { [weak self] result in
+      guard let self else { return }
+
+      DispatchQueue.main.async {
+        self.view?.hideLoader()
+
+        switch result {
+        case let .success(collections):
+          self.collections = collections
+          self.view?.reloadData()
+
+        case let .failure(error):
+          self.view?.showError("Something went wrong")
+        }
+      }
+    }
+  }
+}
+
+// MARK: CatalogPresenter.SortingOption
+
+extension CatalogPresenter {
+  enum SortingOption {
+    case name
+    case count
+  }
+}
+
+// MARK: CatalogPresenter.Constants
+
+extension CatalogPresenter {
+  private enum Constants {}
+}
---
+++ FakeNFT/Scenes/Catalog/View/CatalogViewProtocol.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+protocol CatalogViewProtocol: AnyObject {
+  func reloadData()
+  func presentSortingOptions()
+  func showLoader()
+  func hideLoader()
+  func showError(_ message: String)
+}
