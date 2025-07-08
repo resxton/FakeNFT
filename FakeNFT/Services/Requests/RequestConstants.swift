79b6b0a97c64b4734e9f428b9458bf92093eabf4
maksdemidchik <maksimdemidcik37@gmail.com>
2025-07-08 12:30:39 +0500

    add token

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,5 +1,5 @@
 enum RequestConstants {
   static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
   #warning("Insert your token here")
-  static let token = ""
+  static let token = "e7c7890f-5211-4f70-b43b-d23d82713a3b"
 }
